43485e790d420847918e86ad32a8814da49bf494
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-09-20 20:08:35 +0900

    Chapter3で、変数同士の足し算についてやった。Chapter4で、変数を2倍させたり2乗させたりした。

---
+++ Javascript/JS/JavaScript3.js
@@ -0,0 +1,9 @@
+//文字列と変数を連結して、二つの値を足し算し、出力例通りに出力する。
+
+var FirstAmount  = 10;
+var SecondAmount = 20;
+
+//加算した結果を格納
+var ResultAmount = FirstAmount + SecondAmount;
+
+alert(FirstAmount + "+" + SecondAmount + "=" + ResultAmount);
---
+++ Javascript/JS/JavaScript4.js
@@ -0,0 +1,9 @@
+//変数を二倍した値と、2乗した値を文字列を変数と連結して、出力例通りに出力する
+var BaseAmount = 10;
+
+var TwiceAmount     = BaseAmount *  2;//BaseAmountを2倍した値
+var SquarinigAmount = BaseAmount ** 2;//BaseAmountを2乗した値
+
+
+alert(BaseAmount + " を2倍した値 : " + TwiceAmount +"\n"
+    + BaseAmount + " を2乗した値 : " + SquarinigAmount);
