8246e6ba1394dd65780870cdd72a2e2ba2900dfa
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-09-21 21:21:02 +0900

    丁半博打

--- Javascript/JS/JavaScript9.js
+++ Javascript/JS/JavaScript9.js
@@ -1,2 +1,16 @@
-var RandomAmount = 100;
-alert(RandomAmount);
+//ランダムで抽出された値が、奇数なら「半」偶数なら「丁」の丁半博打を作る。
+
+var RandomAmount = Math.floor(Math.random() * 100);
+
+
+//偶数なら「丁」、奇数なら「半」
+if(RandomAmount % 2 == 0)
+{
+	alert("ランダムで選ばれた値 : " + RandomAmount + "\n"
+	+ "結果 : 丁");
+}
+else
+{
+	alert("ランダムで選ばれた値 : " + RandomAmount + "\n"
+	+ "結果 : 半");
+}
