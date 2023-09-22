1ceabe12337b9b74f28266c42dc4e1fc0852affb
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-09-22 20:26:11 +0900

    ボタンのイベントハンドラーについて

---
+++ Javascript/JS/JavaScript17.js
@@ -0,0 +1,9 @@
+//onClickイベントハンドラ
+
+
+//0～100までのランダムな値を取得してるとこ
+function getRandomNumber()
+{
+	const SetRandomNumber = Math.floor(Math.random() * 100);
+	alert("ランダムな値 : " + SetRandomNumber);	
+} 
