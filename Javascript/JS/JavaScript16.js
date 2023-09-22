7dc5435b4c1be888265f0a438b689cd412e86edc
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-09-22 20:16:01 +0900

    配列の要素の最大値を表示

--- Javascript/JS/JavaScript16.js
+++ Javascript/JS/JavaScript16.js
@@ -1,14 +1,13 @@
 /*引数に渡された配列の中の最大値を戻す関数を作成し、
 引数の中から最大値を見つけ出し、呼び出し元へ返した値を出力する。*/
 
-const array = [30,23,13,49,42];
-const maxValue = 0;
 
+//配列内の最大値を求める
 function getMaxValue(nums)
 {	
-	//配列内の最大値を求める
 	for(let i = 0; i < array.length; i++)
 	{
+		//最大値を更新してく
 		if(nums  < array[i])
 		{
 			nums = array[i];
@@ -18,5 +17,8 @@ function getMaxValue(nums)
 	return nums;
 } 
 
+
+const array    = [30,23,13,49,42];
+const maxValue = 0;
 
 alert("配列30,23,19,49,42の最大値 : " + getMaxValue(maxValue));
